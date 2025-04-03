17de2eb0b347b6ee5d72579241f34020e4e3e647
Enzo Gabriel Previtale Silva <49513408876@>
2025-04-03 17:00:28 -0300

    excel

--- 2read.py
+++ 2read.py
@@ -1,15 +1,26 @@
 import pandas as pd
+import openpyxl
 
 # Lendo um DataFrame em formato csv
-
 tabela = pd.read_csv("Data/pandas.csv")
 
 # Imprimindo partes de um DataFrame
-'''
+"""
 print(tabela.head()) # Começo
 print(tabela.tail()) # Fim
 # O número de linhas a ser imprimido é o parâmetro da função
-'''
+"""
+
+# Convertendo para excel
+tabela.to_excel("tabela.xlsx", sheet_name="tabela", index=False)
 
 # Imprimindo um DataFrame
-print(tabela) # Primeiras e últimas 5 linhas
+# print(tabela) # Primeiras e últimas 5 linhas
+
+# Imprimindo tipos dos dados
+#print(tabela.dtypes)
+
+# Informações técnicas de um DataFrame
+# print(tabela.info())
+
+
